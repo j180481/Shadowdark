b3ee5f8c14e5ab81ebd737ba2deb278297d8f1fb
J180481 <J180481@tafe.wa.edu.au>
2026-06-12 00:46:13 +0800

    Refactor: refactored more code for link with flake8 test

--- Shadowdark_level_0.py
+++ Shadowdark_level_0.py
@@ -2,9 +2,12 @@ import random
 import math
 
 # Chose Background
-Background = random.choice(['Urchin', 'Wanted', 'Cult Initiate', "Thieves' Guild", 'Banished', 'Orphaned',
-                            "Wizard's Apprentice", 'Jeweler', 'Herbalist', 'Barbarian', 'Mercenary', 'Sailor',
-                            'Acolyte', 'Soldier', 'Ranger', 'Scout', 'Minstrel', 'Scholar', 'Noble', 'Chirurgeon'])
+Background = random.choice(['Urchin', 'Wanted', 'Cult Initiate',
+                            "Thieves' Guild", 'Banished', 'Orphaned',
+                            "Wizard's Apprentice", 'Jeweler', 'Herbalist',
+                            'Barbarian', 'Mercenary', 'Sailor',
+                            'Acolyte', 'Soldier', 'Ranger', 'Scout',
+                            'Minstrel', 'Scholar', 'Noble', 'Chirurgeon'])
 
 
 def roll_stat_or_gold(is_gold=False):
@@ -16,7 +19,7 @@ def roll_stat_or_gold(is_gold=False):
 
 
 def get_stat_mod(stat):
-    return math.floor((stat-10) / 2)
+    return math.floor((stat - 10) / 2)
 
 
 # Roll Stats
@@ -28,18 +31,23 @@ WIS = roll_stat_or_gold()
 CHA = roll_stat_or_gold()
 
 Ancestry = random.choice([
-    {'name': 'Dwarf', 'talent': 'Stout. Start with +2 '
-                                'HP (Included). Roll your hit point gains with advantage.'},
-    {'name': 'Elf', 'talent': 'Farsight. You get a +1 bonus to attack '
-                              'rolls with ranged weapons or a +1 bonus to spellcasting checks.'},
-    {'name': 'Half-Orc', 'talent': 'Mighty. You have a '
-                                   '+1 bonus to attack and damage rolls with melee weapons.'},
-    {'name': 'Halfling', 'talent': 'Stealthy. '
-                                   'Once per day, you can become invisible for 3 rounds.'},
-    {'name': 'Goblin', 'talent': 'Keen Senses. '
-                                 'You cannot be surprised.'},
-    {'name': 'Human', 'talent': 'Ambitious. '
-                                'You gain one additional talent roll at 1st level.'}
+    {'name': 'Dwarf',
+     'talent': 'Stout. Start with +2 HP (Included).'
+               'Roll your hit point gains with advantage.'},
+    {'name': 'Elf',
+     'talent': 'Farsight. You get a +1 bonus to attack rolls with '
+               'ranged weapons or a +1 bonus to spellcasting checks.'},
+    {'name': 'Half-Orc',
+     'talent': 'Mighty. You have a +1 bonus'
+               ' to attack and damage rolls with melee weapons.'},
+    {'name': 'Halfling',
+     'talent': 'Stealthy. Once per day, '
+               'you can become invisible for 3 rounds.'},
+    {'name': 'Goblin',
+     'talent': 'Keen Senses. You cannot be surprised.'},
+    {'name': 'Human',
+     'talent': 'Ambitious. You gain one additional'
+               ' talent roll at 1st level.'}
 ])
 
 # Get HP
