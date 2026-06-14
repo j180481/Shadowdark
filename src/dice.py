60d353f29da926429d41bc0221cb6255f5b69c8d
J180481 <J180481@tafe.wa.edu.au>
2026-06-14 18:50:34 +0800

    documentation: added doscstring commentation to dice.py

--- src/dice.py
+++ src/dice.py
@@ -2,5 +2,21 @@ import random
 
 
 def dice_roller(dice, count):
+    """
+        Dice roller function.
+        Takes dice and roll count and
+        returns sum.
+        Args:
+            dice: <int> basically the "sides"
+            of the dice. Get random number
+            between 1 and the dice int.
+            count: <int> this is how many times
+            we "roll" the dice basically.
+            Get the random number between 1 and
+            dice number COUNT many times.
+
+        Returns:
+            Number: <int> sum of all the rolls.
+    """
 
     return sum([random.randint(1, dice) for i in range(count)])
